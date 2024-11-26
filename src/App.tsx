ac1b94028c8b7879f843a13f0bdfc83fd6df833c
Eugenio Ciccale <eoc@macbookpro.speedport.ip>
2024-11-26 09:30:51 +0100

    *

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles/App.scss';
+
+const App: React.FC = () => {
+  return (
+    <div className="app">
+      <h1>Rect Test</h1>
+    </div>
+  );
+};
+
+export default App;
---
+++ webpack/webpack.dev.js
@@ -0,0 +1,12 @@
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'source-map',
+  devServer: {
+    static: './dist',
+    hot: true,
+    open: true,
+  },
+});
